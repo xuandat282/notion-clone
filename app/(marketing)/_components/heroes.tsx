b26e2043000645c20830baaa19bf94890e796a4f
Dat Xuan Huynh <xuandat282@gmail.com>
2023-10-18 21:16:10 +0700

    ui: landing page

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -1,23 +1,35 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export const Heroes = () => {
   return (
-    <div className="flex flex-col items-center justify-center max-w-5xl">
-      <div className="flex items-center">
-        <div className="relative w-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+    <div className='flex flex-col items-center justify-center max-w-5xl'>
+      <div className='flex items-center'>
+        <div className='relative w-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]'>
           <Image
-            src="/documents.png"
-            alt="Documents"
+            src='/documents.png'
+            alt='Documents'
             fill
-            className="object-contain"
+            className='object-contain dark:hidden'
+          />
+          <Image
+            src='/documents-dark.png'
+            alt='Documents'
+            fill
+            className='object-contain hidden dark:block'
           />
         </div>
-        <div className="relative h-[400px] w-[400px] hidden md:block">
+        <div className='relative h-[400px] w-[400px] hidden md:block'>
           <Image
-            src="/reading.png"
-            alt="reading"
+            src='/reading.png'
+            alt='reading'
             fill
-            className="object-contain"
+            className='object-contain dark:hidden'
+          />
+          <Image
+            src='/reading-dark.png'
+            alt='reading'
+            fill
+            className='object-contain hidden dark:block'
           />
         </div>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Notion",
+  title: 'Notion',
   description:
-    "The all-in-one workspace for your notes, tasks, wikis, and databases.",
+    'The all-in-one workspace for your notes, tasks, wikis, and databases.',
 };
 
 export default function RootLayout({
@@ -16,8 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html
+      lang='en'
+      suppressHydrationWarning
+    >
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+          storageKey='theme'
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
